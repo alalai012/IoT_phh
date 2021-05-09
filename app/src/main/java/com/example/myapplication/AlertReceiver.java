beb8028df03c0bda6bb815f6d4dd1a960f8df6aa
psj <tjswn0125@naver.com>
2021-05-10 00:01:37 +0900

    https://appdpmtstore.tistory.com/4 알람 예제 추가

---
+++ app/src/main/java/com/example/myapplication/AlertReceiver.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+//알람 시간이 되었을 때 동작할 기능
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1,nb.build());
+    }
+}
